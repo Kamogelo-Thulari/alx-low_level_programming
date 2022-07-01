aeee969e6739409a24ceb9e1d4ba8bdc2c7bed42
Kamogelo-Thulari <kamogelothulari@gmail.com>
2022-06-30 17:06:15 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * Programming is like building a  multilimgual puzzle, that was a  text from alx.
- *  return 0
+/
+ * main - Entry point
+ * Return: Always 0 (sucess)
  */
-int main (void)
+int main(void)
 {
-	put ("\"Programming is like building a multilingual puzzle")
-	       return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
