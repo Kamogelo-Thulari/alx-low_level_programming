49faf9e7dc1aa84866a5659c4b2c4b75c27f9fae
Kamogelo-Thulari <kamogelothulari@gmail.com>
2022-07-04 03:21:10 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,5 @@ int main(void)
 	printf("size of a long int: %lu byte(s)\n", sizeof(I));
 	printf("size of a long long int: %lu byte(s)\n", sizeof(II));
 	printf("size of a float: %lu byte(s)\n", sizeof(d));
-	printf("size of a char: %lu byte(s)\n", sizeof(c));
 	return (0);
 }
