d6d010c5f09ef663d1b08c8b91e65daa709a947a
Kamogelo-Thulari <kamogelothulari@gmail.com>
2022-07-05 05:26:02 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-*main - Print _putchar
-*
+*main - Print putchar
 *Return: Always 0(Success)
 */
 int main(void)
@@ -16,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
